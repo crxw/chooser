7df7eccc90a9c4c3546a0f97e44e021ffd35c978
Dale E. Moore <DaleEMoore@gMail.Com>
2014-08-14 22:42:40 -0500

    Seems to meet all the original design goals.

--- simple.py
+++ simple.py
@@ -7,3 +7,13 @@
 
 __author__ = 'dalem'
 
+thinking_of = [3, 7]
+
+while True:
+    guess = input("What number am I thinking of between 1 and 10? ")
+
+    if guess in thinking_of:
+        print("Congratulations I was thinking of " + ''.join(str(e) for e in thinking_of))
+        break
+    else:
+        print("Nope, try again.")
