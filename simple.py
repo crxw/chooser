9a8a87a0bfa1fb101223cf2e643014a7eac50b1f
Dale E. Moore <DaleEMoore@gMail.Com>
2014-08-14 22:35:58 -0500

    Initial commit

---
+++ simple.py
@@ -0,0 +1,9 @@
+# chooser or Choose-Number
+
+# Set correct numbers 3 and 7.
+# Ask user, what number am I thinking of.
+# If it's 3 or 7, congratulate them.
+# Let them try again.
+
+__author__ = 'dalem'
+
