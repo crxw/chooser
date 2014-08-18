5187ba8b45edff016d6373304484a4897ed0503c
crxw <captainaptos1@gmail.com>
2014-08-17 19:41:06 -0500

    Update simple.py

--- simple.py
+++ simple.py
@@ -8,13 +8,15 @@
 __author__ = 'dalem'
 
 # TODO; make thinking_of two values randomly assigned on each execution.
-thinking_of = [3, 5, 7]
+arbitrary_change = [3, 5, 7]
 
 while True:
     guess = input("What number am I thinking of between 1 and 10? ")
 
-    if guess in thinking_of:
-        print("Congratulations I was thinking of " + ''.join(str(e) for e in thinking_of))
+    if guess in arbitrary_change:
+        print("Congratulations I was thinking of " + ''.join(str(e) for e in arbitrary_change))
         break
     else:
         print("Nope, try again.")
+# The program prints all correct numbers upon entering the 3, 5, or 7, instead of just the number by itself.
+# I will try to fix it.
